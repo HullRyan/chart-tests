2561ecd5562236cdabe4b6bdbba91998c75d695e
HullRyan <hull.ryanc@gmail.com>
2022-01-31 19:45:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vaampq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vaampq)
---
+++ index.js
@@ -0,0 +1,50 @@
+// Import stylesheets
+import './style.css';
+
+const Chart = require('chart.js');
+
+const ctx = 'myChart';
+
+const myChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.7)',
+              'rgba(54, 162, 235, 0.7)',
+              'rgba(255, 206, 86, 0.7)',
+              'rgba(75, 192, 192, 0.7)',
+              'rgba(153, 102, 255, 0.7)',
+              'rgba(255, 159, 64, 0.7)'
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  options: {
+      scales: {
+        x: {
+          grid: {
+            color: 'none',
+            borderColor: 'white'
+          }
+        },
+        y: {
+          grid: {
+            color: 'none',
+            borderColor: 'white'
+          }
+        }
+      }
+  }
+});
